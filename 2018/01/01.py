e2768f1c9dd64a455f0fb9a6e31b8a480b3557bd
Wayne Werner <wayne@waynewerner.com>
2018-12-02 02:50:28 +0000

    Day 1, part 2

--- 2018/01/01.py
+++ 2018/01/01.py
@@ -1,4 +1,24 @@
 from ast import literal_eval
 
+seen_frequencies = set()
+found = False
+
 with open('input.txt') as f:
-    print(sum(literal_eval(freq_change.strip()) for freq_change in f))
+    frequency = 0
+    for freq_change in f:
+        amount = literal_eval(freq_change)
+        frequency += amount
+        if not found and frequency in seen_frequencies:
+            found = True
+            print('Second time seen:', frequency)
+        seen_frequencies.add(frequency)
+    print('Final frequency:', frequency)
+
+    while not found:
+        f.seek(0)
+        for freq_change in f:
+            amount = literal_eval(freq_change)
+            frequency += amount
+            if not found and frequency in seen_frequencies:
+                found = True
+                print('Second time seen:', frequency)
