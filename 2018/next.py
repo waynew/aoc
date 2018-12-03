d878dce9e8f3633d0b5b1123c752e2074e3874bd
Wayne Werner <wayne@waynewerner.com>
2018-12-03 04:59:10 +0000

    Added nextify

---
+++ 2018/next.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import sys
+import os
+from pathlib import Path
+
+try:
+    day = Path(sys.argv[1]).absolute()
+except IndexError:
+    sys.exit('Usage: next.py DAY')
+
+print(day)
+os.makedirs(day, exist_ok=True)
+with open('template.py') as f:
+    with open(day / 'template.py', 'w') as outf:
+        outf.write(f.read())
+    with open(day / 'one.txt', 'w'):
+        pass
+    with open(day / 'two.txt', 'w'):
+        pass
+
+    input_txt = day / 'input.txt'
+    if input_txt.exists():
+        input_txt.unlink()
+    os.symlink(day / 'one.txt', input_txt)
