5f63f12a4b0a9e5770e364dd67c12568b2047ce0
Wayne Werner <wayne@waynewerner.com>
2018-12-18 21:42:41 -0600

    Day 11 is actually done! Slowly!

--- 2018/11/11.py
+++ 2018/11/11.py
@@ -52,32 +52,80 @@ def calc_power(x, y, grid_serial):
     return power_level
 
 
-def smaller_grid_power(x, y, grid):
+def smaller_grid_power(x, y, grid, size=4):
+    #print(x, y, size)
     real_x = x-1
     real_y = y-1
     total_power = 0
-    for y in range(real_y, real_y+3):
-        for x in range(real_x, real_x+3):
-            #print(f'{grid[y][x]!s:>2}', end='  ')
-            total_power += grid[y][x]
-        #print()
-    return total_power
+#    if real_x == 32 and real_y == 44:
+#        for col in range(real_y, real_y+size):
+#            for row in range(real_x, real_x+size):
+#                print(f'{grid[col][row]!s:>2}', end='  ')
+#            print()
+    if size < 1:
+        return 0
+    elif size == 1:
+        #print('power:', grid[real_y][real_x])
+        return grid[real_y][real_x]
+    else:
+        top = sum(grid[real_y][real_x:real_x+size])
+        #print(x,y)
+        #print(real_y+size-1)
+        bottom = sum(grid[real_y+size-1][real_x:real_x+size])
+        left = 0
+        right = 0
+        for col in range(real_y+1, real_y+size-1):
+            left += grid[col][real_x]
+            right += grid[col][real_x+size-1]
+        #print('----')
+        #print(grid[real_y][real_x:real_x+size])
+        #print(grid[real_y+size-1][real_x:real_x+size])
+        #print(left, right)
+        #print('====')
+
+        return top+bottom+left+right+smaller_grid_power(x+1, y+1, grid, size-2)
 
 
 def do_it(grid_serial, width=300, height=300):
     # 1-based coordinate system
-    maximum_grid = {'coord': None, 'power': -sys.maxsize}
+    maximum_grid = {'coord': None, 'power': -sys.maxsize, 'size': None}
     grid = [[0]*width for _ in range(height)]
     for x in range(1, width+1):
         for y in range(1, height+1):
             cell_power = calc_power(x,y, grid_serial)
             grid[y-1][x-1] = cell_power
             if x > 3 and y > 3:
-                grid_power = smaller_grid_power(x-3, y-3, grid)
+                grid_power = smaller_grid_power(x-3, y-3, grid, size=3)
                 if grid_power > maximum_grid['power']:
                     maximum_grid['power'] = grid_power
                     maximum_grid['coord'] = (x-3, y-3)
+    return grid, maximum_grid
 
+
+def do_it_two(grid_serial, width=300, height=300):
+    maximum_grid = {'coord': None, 'power': -sys.maxsize, 'size': None}
+    grid = [[0]*width for _ in range(height)]
+    # 1-based coord system
+    for x in range(1, width+1):
+        for y in range(1, height+1):
+            cell_power = calc_power(x,y, grid_serial)
+            grid[y-1][x-1] = cell_power
+
+
+    size = 300
+    max_xy = (0,0)
+    for size in range(300, 0, -1):
+    #for size in range(12, 17):
+        for y in range(height-size):
+            for x in range(width-size):
+                max_xy = (x, y)
+                grid_power = smaller_grid_power(x, y, grid, size=size)
+                if grid_power > maximum_grid['power']:
+                    maximum_grid['power'] = grid_power
+                    maximum_grid['coord'] = (x,y)
+                    maximum_grid['size'] = size
+        print('Processed size:', size, end='    \r')
+    print()
     return grid, maximum_grid
 
 
@@ -86,29 +134,50 @@ def result_to_xy(result):
     return f'{x},{y}'
 
 
-print(f'{" Sample ":*^40}')
-assert calc_power(3,5,8) == 4
-assert calc_power(122,79,57) == -5
-assert calc_power(217,196,39) == 0
-assert calc_power(101,153,71) == 4
+def result_to_xysize(result):
+    x,y = result['coord']
+    return f'{x},{y},{result["size"]}'
 
-assert do_it(8)[0][5-1][3-1] == 4
-print('*'*10)
-result = do_it(18)
-with open('output.txt', 'w') as f:
-    printf = functools.partial(print, file=f)
-    for row in result[0]:
-        for x in row:
-            printf(f'{x!s:>3}', end='')
-        printf()
-print('What', result_to_xy(do_it(42)))
-print(f'{" End Sample ":*^40}')
-print('\n')
 
-print(f'{" Live ":*^40}')
-data = get_data(day=DAY)
-result = do_it(int(data))
-print(result_to_xy(result))
-#submit1(result_to_xy(result))
-#submit2(result)
-print(f'{" Done ":*^40}')
+def grid_to_output(grid):
+    with open('output.txt', 'w') as f:
+        printf = functools.partial(print, file=f)
+        for row in grid:
+            for x in row:
+                printf(f'{x if x > 0 else ""!s:>3}', end='')
+            printf()
+
+def main():
+    result = do_it(18)
+    print(result_to_xy(result))
+    print(f'{" Sample ":*^40}')
+    assert calc_power(3,5,8) == 4
+    assert calc_power(122,79,57) == -5
+    assert calc_power(217,196,39) == 0
+    assert calc_power(101,153,71) == 4
+
+    assert do_it(8)[0][5-1][3-1] == 4
+    print('*'*10)
+    result = do_it(18)
+    print('What', result_to_xy(do_it(42)))
+    print(f'{" End Sample ":*^40}')
+    print('\n')
+
+    print(f'{" Live ":*^40}')
+    data = get_data(day=DAY)
+    grid_serial = int(data)
+    result = do_it(grid_serial)
+    grid_to_output(result[0])
+    print(result_to_xy(result))
+    #submit1(result_to_xy(result))
+    grid, result = do_it_two(grid_serial)
+    print(result_to_xysize(result))
+    #submit2(result_to_xysize(result))
+    print(f'{" Done ":*^40}')
+
+    #print('sample again')
+    #grid, result = do_it_two(42)
+    #print('Answer:', result_to_xysize(result))
+    #print(smaller_grid_power(90,269, grid, size=16))
+
+main()
