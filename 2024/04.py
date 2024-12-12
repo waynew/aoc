5ff326cac781839034f53e6aaba6fe1d24cd5ded
Wayne Werner <wayne@waynewerner.com>
2024-12-11 19:53:11 -0600

    y u no work day4?

--- 2024/04.py
+++ 2024/04.py
@@ -4,64 +4,99 @@ from collections import Counter
 DAY=4
 
 data = get_data(year=2024, day=DAY)
-sample_data =  data = '''\
-MMMSXXMASM                                                                 
-MSAMXMSMSA                                                                 
-AMXSXMAAMM                                                                 
-MSAMASMSMX                                                                 
-XMASAMXAMM                                                                 
-XXAMMXXAMA                                                                 
-SMSMSASXSS                                                                 
-SAXAMASAAA                                                                 
-MAMMMXMMMM                                                                 
+sample_data = '''\
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
 MXMXAXMASX\
 '''
 data = data.split()
 
-def find_xmas(row, col, data):
+def find_xmas(row, col, data, other_data):
     count = 0
     # Look right
-    if col+3 < len(data[row]) and data[row][col:col+4] == 'XMAS':
+    if col+3 < len(data[row]) and data[row][col+1] == 'M' and data[row][col+2] == 'A' and data[row][col+3] == 'S':
         count += 1
+        other_data[row][col] = red('X')
+        other_data[row][col+1] = red('M')
+        other_data[row][col+2] = red('A')
+        other_data[row][col+3] = red('S')
     # Look left
-    if col > 3 and data[row][col-4:col] == 'XMAS':
+    if col > 3 and data[row][col-1] == 'M' and data[row][col-2] == 'A' and data[row][col-3] == 'S':
         count += 1
+        other_data[row][col] = red('X')
+        other_data[row][col-1] = red('M')
+        other_data[row][col-2] = red('A')
+        other_data[row][col-3] = red('S')
     # Look up
     if row > 3 and data[row-1][col] == 'M' and data[row-2][col] == 'A' and data[row-3][col] == 'S':
         count += 1
+        other_data[row][col] = red('X')
+        other_data[row-1][col] = red('M')
+        other_data[row-2][col] = red('A')
+        other_data[row-3][col] = red('S')
     # Look down
     if row+3 < len(data) and data[row+1][col] == 'M' and data[row+2][col] == 'A' and data[row+3][col] == 'S':
         count += 1
+        other_data[row][col] = red('X')
+        other_data[row+1][col] = red('M')
+        other_data[row+2][col] = red('A')
+        other_data[row+3][col] = red('S')
     # Look up left
-    if col > 3 and row > 3 and data[row-1][col-1] == 'M' and data[row-2][col-2] == 'A' and data[row-3][col-3] == 'S':
+    if col > 2 and row > 2 and data[row-1][col-1] == 'M' and data[row-2][col-2] == 'A' and data[row-3][col-3] == 'S':
         count += 1
+        other_data[row][col] = red('X')
+        other_data[row-1][col-1] = red('M')
+        other_data[row-2][col-2] = red('A')
+        other_data[row-3][col-3] = red('S')
     # Look up right
     if col+3 < len(data[row]) and row > 3 and data[row-1][col+1] == 'M' and data[row-2][col+2] == 'A' and data[row-3][col+3] == 'S':
         count += 1
+        other_data[row][col] = red('X')
+        other_data[row-1][col+1] = red('M')
+        other_data[row-2][col+2] = red('A')
+        other_data[row-3][col+3] = red('S')
     # Look down right
-    elif col+3 < len(data[row]) and row+3 < len(data) and data[row+1][col+1] == 'M' and data[row+2][col+2] == 'A' and data[row+3][col+3] == 'S':
+    if col+3 < len(data[row]) and row+3 < len(data) and data[row+1][col+1] == 'M' and data[row+2][col+2] == 'A' and data[row+3][col+3] == 'S':
         count += 1
+        other_data[row][col] = red('X')
+        other_data[row+1][col+1] = red('M')
+        other_data[row+2][col+2] = red('A')
+        other_data[row+3][col+3] = red('S')
     # Look down left
-    elif col > 3 and row+3 < len(data):
-        for i in range(4):
-            s = list(data[row+i])
-            char = s[col]
-            s[col] = f'[31;m{s[col]}[0m'
-            data[row+i] = ''.join(s)
-        if data[row+1][col-1] == 'M' and data[row+2][col-2] == 'A' and data[row+3][col-3] == 'S':
-            count += 1
+    if col > 3 and row+3 < len(data) and data[row+1][col-1] == 'M' and data[row+2][col-2] == 'A' and data[row+3][col-3] == 'S':
+        count += 1
+        other_data[row][col] = red('X')
+        other_data[row+1][col-1] = red('M')
+        other_data[row+2][col-2] = red('A')
+        other_data[row+3][col-3] = red('S')
     return count
 
+
+def red(letter):
+    return f'[31;m{letter}[0m'
+
 def part_one(data):
-    data = [row.split() for row in data]
+    data = [list(row) for row in data]
+    import copy
+    other_data = copy.deepcopy(data)
     count = 0
     for i, row in enumerate(data):
-        #print(row)
+        print(row)
         for j, letter in enumerate(row):
             if letter == 'X':
-                count += find_xmas(row=i, col=j, data=data)
+                #other_data[i][j] = red('X')
+                count += find_xmas(row=i, col=j, data=data, other_data=other_data)
     print()
     print('\n'.join(''.join(d) for d in data))
+    print('*'*20)
+    print('\n'.join(''.join(d) for d in other_data))
     print(count)
     #submit(count, part="a", day=DAY, year=2024, reopen=False)
 
